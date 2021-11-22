bbf95df8b06161b595717ba1d918e6cb03c727a6
Freddie Mixell <fmixell@gmail.com>
2021-11-22 16:57:20 -0500

    remove dependency

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import FocusLock from "react-focus-lock";
-import uniqid from "uniqid";
 import { ModalType, ModalStore } from "../core/types";
 import { useModalStore } from "../core/store";
 
@@ -14,7 +13,11 @@ const dialogProps = {
 >;
 
 // Placing outside of the component to avoid re-rendering.
-const selector = (state: ModalStore) => ({ deactivate: state.deactivate });
+const selector = (state: ModalStore) => state.deactivate;
+
+// Since only one modal can be active at a time, we can use a single class for all modals.
+const titleId = 'modal-title';
+const descId = 'modal-desc';
 
 export const Modal = ({
   id,
@@ -23,23 +26,20 @@ export const Modal = ({
   description,
   actions = []
 }: ModalType) => {
-  let { deactivate } = useModalStore(selector);
-
-  // Storing accessablity related dynamic id's in ref to avoid re-rendering.
-  const titleIdRef = React.useRef(uniqid("title-"));
-  const descIdRef = React.useRef(uniqid("desc-"));
+  let deactivate = useModalStore(selector);
 
   // Using useCallback because this is a dependency of useEffect which would cause re-rendering.
   const closeDialog = React.useCallback(() => deactivate(id), [id, deactivate]);
 
   if (title) {
-    dialogProps["aria-labelledby"] = titleIdRef.current;
+    dialogProps["aria-labelledby"] = titleId;
   }
 
   if (description) {
-    dialogProps["aria-describedby"] = descIdRef.current;
+    dialogProps["aria-describedby"] = descId;
   }
 
+  // Allow users to close the modal with ESC key.
   React.useEffect(() => {
     const close = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
@@ -58,8 +58,8 @@ export const Modal = ({
         <div className="modal__window">
           {(title || description) && (
             <header className="modal__head">
-              {title && <h2 id={titleIdRef.current}>{title}</h2>}
-              {description && <p id={descIdRef.current}>{description}</p>}
+              {title && <h2 id={titleId}>{title}</h2>}
+              {description && <p id={descId}>{description}</p>}
             </header>
           )}
           <div className="modal__content">{children}</div>
