cde69cd4faf8fa61d8f005128a502878863274c1
Freddie Mixell <fmixell@gmail.com>
2021-11-22 17:12:57 -0500

    optimize modal router

--- src/components/ModalRouter.tsx
+++ src/components/ModalRouter.tsx
@@ -1,25 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useModalStore } from "../core/store";
-import { ModalRouterProps } from "../core/types";
+import { ModalRouterProps, ModalStore } from "../core/types";
+
+const selector = (state: ModalStore) => state.modals;
 
 // This can be placed anywhere in app because it uses a portal.
 export const ModalRouter = ({ children, modalRoot }: ModalRouterProps) => {
-  let activeModal = null;
-  let modals = useModalStore(React.useCallback((state) => state.modals, []));
-  let toRender = React.Children.toArray(children).filter((child) => {
-    if (React.isValidElement(child)) {
-      return modals.includes(child.props?.id);
-    }
-    return false;
-  });
+  let modals = useModalStore(selector);
+  let filteredModals = React.Children
+    .toArray(children)
+    .filter((child) => React.isValidElement(child) && modals.includes(child.props?.id));
 
-  // Setting active modal in regular var to prevent extra rendering.
-  if (toRender.length > 0) {
-    activeModal = toRender[0];
-  }
+  // Destructure the first modal so we only ever show one at a time.
+  const [firstModal = null] = filteredModals;
 
   return modalRoot
-    ? ReactDOM.createPortal(<React.Fragment>{activeModal}</React.Fragment>, modalRoot)
+    ? ReactDOM.createPortal(<React.Fragment>{firstModal}</React.Fragment>, modalRoot)
     : null;
 };
