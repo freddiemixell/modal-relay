a6e89e3dbddb1a64c20db2e53cb4278d2546ac43
Freddie Mixell <fmixell@gmail.com>
2021-11-27 12:52:30 -0500

    make modal composable

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import FocusLock from 'react-focus-lock'
-import { ModalType, ModalStore } from '../core/types'
+import { ModalType, ModalStore, ActionButton } from '../core/types'
 import { useModalStore } from '../core/store'
+
+export { FocusLock }
+
+type DefaultDialogProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>
+
+type DefaultDivProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>
 
 const dialogProps = {
   className: 'modal',
   role: 'dialog',
   'aria-modal': 'true'
-} as React.DetailedHTMLProps<
-  React.HTMLAttributes<HTMLDivElement>,
-  HTMLDivElement
->
+} as DefaultDialogProps
 
 // Placing outside of the component to avoid re-rendering.
-const selector = (state: ModalStore) => state.deactivate;
+const selector = (state: ModalStore) => state.deactivate
 
 // Since only one modal can be active at a time, we can use a single ID for all modals.
-const titleId = 'modal-title';
-const descId = 'modal-desc';
+const titleId = 'modal-title'
+const descId = 'modal-desc'
 
 export const Modal = ({
   id,
@@ -52,31 +61,89 @@ export const Modal = ({
 
   return (
     <FocusLock returnFocus>
-      <div {...dialogProps}>
-        <div className="modal__window">
-          {(title || description) && (
-            <header className="modal__head">
-              {title && <h2 id={titleId}>{title}</h2>}
-              {description && <p id={descId}>{description}</p>}
-            </header>
-          )}
-          <div className="modal__content">{children}</div>
-          <footer className="modal__actions">
-            {actions.length > 0
-              ? actions.map((action) => (
-                  <button
-                    key={action.label}
-                    className="button button--primary"
-                    onClick={action.onClick}
-                  >
-                    {action.label}
-                  </button>
-                ))
-              : null}
-          </footer>
-        </div>
-        <div onClick={closeDialog} className="modal__mask" />
-      </div>
+      <Dialog>
+        <ModalWindow>
+          <ModalHeader>
+            {title && <h2 id={titleId}>{title}</h2>}
+            {description && <p id={descId}>{description}</p>}
+            <button onClick={() => deactivate(id)}>x</button>
+          </ModalHeader>
+          <ModalContent>{children}</ModalContent>
+          <Actions actions={actions} />
+        </ModalWindow>
+        <ModalMask onClose={closeDialog} />
+      </Dialog>
     </FocusLock>
+  )
+}
+
+export const Dialog = ({ children, ...props }: DefaultDialogProps) => {
+  return (
+    <div {...dialogProps} {...props}>
+      {children}
+    </div>
+  )
+}
+
+type ModalWindowProps = DefaultDivProps
+
+export const ModalWindow = ({ children, ...props }: ModalWindowProps) => {
+  return (
+    <div className='modal__window' {...props}>
+      {children}
+    </div>
+  )
+}
+
+type ModalContentProps = DefaultDivProps
+
+export const ModalContent = ({ children, ...props }: ModalContentProps) => {
+  return (
+    <div className='modal__content' {...props}>
+      {children}
+    </div>
+  )
+}
+
+type ModalMaskProps = { onClose: () => void }
+
+export const ModalMask = ({
+  onClose,
+  ...props
+}: ModalMaskProps & DefaultDivProps) => {
+  return <div onClick={onClose} className='modal__mask' {...props} />
+}
+
+type ModalHeaderProps = DefaultDivProps
+
+export const ModalHeader = ({ children, ...props }: ModalHeaderProps) => {
+  return (
+    <header className='modal__head' {...props}>
+      {children}
+    </header>
+  )
+}
+
+type ActionsProps = { actions: ActionButton[] }
+
+export const Actions = ({
+  actions,
+  ...props
+}: ActionsProps & DefaultDivProps) => {
+  if (actions.length === 0) {
+    return null
+  }
+  return (
+    <footer className='modal__actions' {...props}>
+      {actions.map((action) => (
+        <button
+          key={action.label}
+          className='button button--primary'
+          onClick={action.onClick}
+        >
+          {action.label}
+        </button>
+      ))}
+    </footer>
   )
 }
