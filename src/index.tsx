34f8285783809fbd2dcf8b2e15da0c47542387a3
Freddie Mixell <fmixell@gmail.com>
2021-11-27 12:51:48 -0500

    prettier module export

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
-import { Modal } from './components/Modal'
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalWindow,
+  ModalMask,
+  Dialog,
+  FocusLock
+} from './components/Modal'
 import { ModalRelay } from './components/ModalRelay'
 import { ModalLink } from './components/ModalLink'
 import { useModalStore } from './core/store'
 
-export { Modal, ModalRelay, ModalLink, useModalStore }
+export {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalWindow,
+  ModalMask,
+  Dialog,
+  FocusLock,
+  ModalRelay,
+  ModalLink,
+  useModalStore
+}
