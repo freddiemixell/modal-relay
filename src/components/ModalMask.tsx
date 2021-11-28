0504e8fe5e72d7211285af997bce4cbf2045546e
Freddie Mixell <fmixell@gmail.com>
2021-11-28 11:46:45 -0500

    modal mask with tailwind support

---
+++ src/components/ModalMask.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+type DefaultDivProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>
+
+type ModalMaskProps = { onClose: () => void, tailwind?: boolean } & DefaultDivProps
+
+const TAILWIND_MASK_DEFAULT = 'fixed top-0 left-0 h-full w-full z-40 bg-gray-800 bg-opacity-95';
+
+export const ModalMask = ({
+  onClose,
+  className = '',
+  ...props
+}: ModalMaskProps) => {
+  if (props.tailwind) {
+    className += ' ' + TAILWIND_MASK_DEFAULT;
+  }
+  return <div onClick={onClose} className={`modal__mask ${className}`} {...props} />
+}
