af5cb0f0e6954108d0e2cf7d855535ce9db569ce
Freddie Mixell <fmixell@gmail.com>
2021-11-28 11:46:30 -0500

    modal window with tailwind support

---
+++ src/components/ModalWindow.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+type DefaultDivProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>
+
+type ModalWindowProps = { tailwind?: boolean } & DefaultDivProps
+
+const TAILWIND_WINDOW_DEFAULT = "bg-white rounded-lg w-1/2 inline-block fixed left-2/4 top-1/2 z-50";
+
+export const ModalWindow = ({className, style, children, ...props}: ModalWindowProps) => {
+    if (props.tailwind) {
+        className += " " + TAILWIND_WINDOW_DEFAULT;
+        style = {...style, ...{transform: "translate(-50%, -50%)"}}
+    }
+    return (
+        <div className={`modal__window ${className}`} style={style} {...props}>
+            {children}
+        </div>
+    );
+}
