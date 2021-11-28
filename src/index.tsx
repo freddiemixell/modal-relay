bc1a37e0bbdcbb64646857b8081f005aa20b5771
Freddie Mixell <fmixell@gmail.com>
2021-11-28 11:48:47 -0500

    structure export for 2.0.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,23 @@
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalWindow,
-  ModalMask,
-  Dialog,
-  FocusLock
-} from './components/Modal'
 import { ModalRelay } from './components/ModalRelay'
 import { ModalLink } from './components/ModalLink'
+import { Modal } from './components/Modal'
+import { ModalMask } from './components/ModalMask'
+import { ModalWindow } from './components/ModalWindow'
+import { CloseIcon } from './components/CloseIcon'
 import { useModalStore } from './core/store'
+import { useEscapeHatch } from './core/helpers'
+import { ModalStore as MStore } from './core/types'
+
+// Types
+export type ModalStore = MStore
 
 export {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalWindow,
   ModalMask,
-  Dialog,
-  FocusLock,
+  Modal,
+  ModalWindow,
+  CloseIcon,
   ModalRelay,
   ModalLink,
-  useModalStore
+  useModalStore,
+  useEscapeHatch,
 }
