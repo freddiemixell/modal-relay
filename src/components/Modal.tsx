dde6e0adbf13291e14c59a1f7c3a61f3c8953d76
Freddie Mixell <fmixell@gmail.com>
2021-12-27 20:52:18 -0500

    setup remove scroll

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import FocusLock from 'react-focus-lock';
+import { RemoveScroll } from "react-remove-scroll";
 import { DefaultDivProps } from '../core/types';
 
 export const Modal = ({ children, ...props }: DefaultDivProps) => {
   return (
-    <FocusLock returnFocus>
-      <div className="modal" role="dialog" aria-modal="true" {...props}>
-        {children}
-      </div>
+    <FocusLock returnFocus autoFocus>
+      <RemoveScroll allowPinchZoom={false}>
+        <div className="modal" role="dialog" aria-modal="true" {...props}>
+          {children}
+        </div>
+      </RemoveScroll>
     </FocusLock>
   );
 };
