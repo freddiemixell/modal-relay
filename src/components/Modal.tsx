e500663f2f8790b6adf5ef7658ae4f98e92a1b08
Freddie Mixell <fmixell@gmail.com>
2022-02-13 14:06:34 -0500

    bug fix focusProps

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FocusLock from 'react-focus-lock';
 import { DefaultDivProps, FocusLockProps } from '../core/types';
 
-type ModalProps = {focusLockProps: FocusLockProps} & DefaultDivProps;
+type ModalProps = {focusLockProps?: FocusLockProps} & DefaultDivProps;
 
 const defaultFocusProps: FocusLockProps = {
   returnFocus: true,
